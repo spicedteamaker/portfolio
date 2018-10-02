4b6d0d66f54e224f654fe3be401ce069852421c2
teabrewer <teabrewer@teabrewer.com>
2018-10-02 18:36:50 -0400

    devise, login, and navbar bootstrap

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,18 @@
+class PostsController < ApplicationController
+
+  before_action :authenticate_user!
+  def index
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+end
