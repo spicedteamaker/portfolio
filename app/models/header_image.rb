684b3a0b1fcf49bc856ce1777801831520952d2b
teabrewer <teabrewer@teabrewer.com>
2018-10-25 15:18:29 -0400

    Add time and date on posts in the view

--- app/models/header_image.rb
+++ app/models/header_image.rb
@@ -6,7 +6,7 @@ class HeaderImage < ApplicationRecord
     url = "http://localhost:3000" + Rails.application.routes.url_helpers.rails_blob_path(picture, only_path: true)
     image = MiniMagick::Image.new(url)
     unless image.valid?
-      errors.add(:portfolio_post, "only allows valid image files. Are you sure you're uploading a real image?")
+      errors.add(:header_image, "only allows valid image files. Are you sure you're uploading a real image?")
       return false
     end
   end
