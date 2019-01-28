4559739ff752bccaddb96d8ad19d3ef757b00ed5
teabrewer <teabrewer@teabrewer.com>
2019-01-27 19:35:05 -0500

    Overhaul registration

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,13 @@ Rails.application.routes.draw do
 
   get 'header_images/new'
   post 'header_images/create'
-  devise_for :users, controllers: {
-    confirmations: 'users/confirmations',
-    mailer: 'users/mailer',
-    passwords: 'users/passwords',
-    registrations: 'users/registrations',
-    sessions: 'users/sessions',
-    shared: 'users/shared',
-    unlocks: 'users/unlocks'
-  }
+  devise_for :users
+  as :user do
+    get "/sign_in" => "devise/sessions#new" # custom path to login/sign_in
+    #get "/sign_up" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+    put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
