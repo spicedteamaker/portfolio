68571665febb94dcdf2bbcaaf5f4f7ca27d65132
teabrewer <teabrewer@teabrewer.com>
2019-02-11 22:28:53 -0500

    try to fix js

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,20 +1,23 @@
-$(function() {
-  if ($('.pagination').length && $('#blog-posts').length) {
-    $(window).scroll(function() {
-      const url = $('.pagination .next_page').attr('href');
-      if (url && ($(window).scrollTop() > ($(document).height() - $(window).height() - 50))) {
-        $('.pagination').text("Loading...");
-        return $.getScript(url);
-      }
-    });
-    return $(window).scroll();
-  }
-});
 
-$("#toggle-tags-link").click(function() {
-  $(".tag-links-area").toggle();
-});
+$(document).on('turbolinks:load', function() {
+  $(function() {
+    if ($('.pagination').length && $('#blog-posts').length) {
+      $(window).scroll(function() {
+        const url = $('.pagination .next_page').attr('href');
+        if (url && ($(window).scrollTop() > ($(document).height() - $(window).height() - 50))) {
+          $('.pagination').text("Loading...");
+          return $.getScript(url);
+        }
+      });
+      return $(window).scroll();
+    }
+  });
 
-$("#toggle-pinned-link").click(function() {
-  $(".pinned-posts-area").toggle();
+  $("#toggle-tags-link").click(function() {
+    $(".tag-links-area").toggle();
+  });
+
+  $("#toggle-pinned-link").click(function() {
+    $(".pinned-posts-area").toggle();
+  });
 });
